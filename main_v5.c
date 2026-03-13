7857f03d82ea5c5139afb39d2a32a91d630ae6e8
Fernnado-Miguel <fernnandomiguel@gmail.com>
2026-03-13 17:50:31 -0300

    Improve grade system: add input validation, min/max grades, and above/below average count

---
+++ main_v5.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main () {
+    
+    int n;
+    int acima = 0;
+    int abaixo = 0;
+
+    printf("Quantas notas deseja digitar? ");
+    scanf("%d", &n);
+
+    while (n <= 0){
+        printf("Erro!\nDigite um numero positivo: ");
+        scanf("%d", &n);
+    }
+
+    float nota[n];
+    float soma = 0;
+
+    for (int i = 0; i < n; i++) {
+    printf("Digite a %d nota: ", i + 1);
+    scanf("%f", &nota[i]);
+
+    while (nota[i] < 0 || nota[i]> 10){
+        printf("Numero invalido, digite novamente entre 0 e 10: ");
+        scanf("%f", &nota[i]);
+    }
+    
+    soma += nota[i];
+    }
+    float media = soma / n; 
+
+    
+    for (int i = 0; i < n; i++) {
+        if(nota[i] > media) {
+            acima++;
+        }
+        else if (nota[i] < media) {
+            abaixo++;
+        }
+    }
+
+    printf("===== RESULTADO =====\n");
+    printf("Media das notas: %.2f\n", media);
+
+
+    if (media > 6.0){
+        printf("Aluno aprovado!\n");
+    }
+    else {
+        printf("Aluno reprovado!\n");
+    }
+    float maior = nota[0];
+
+    for (int i = 1; i < n; i++){
+        if (nota[i] > maior){
+            maior = nota[i];
+        }
+    }
+    float menor = nota [0];
+    for (int i = 1; i < n; i++){
+        if (nota[i] < menor){
+            menor = nota[i];
+        }
+    }
+    printf("Maior nota: %.2f\n", maior);
+    printf("Menor nota: %.2f\n", menor);
+    printf("Notas acima da media: %d\n", acima);
+    printf("Notas abaixo da media: %d\n", abaixo);
+    return 0;
+}    
